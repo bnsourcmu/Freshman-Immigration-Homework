58446a0b5f5879b7f5694b8ef13e5e7ae6268626
bnsourcmu <bnsour@andrew.cmu.edu>
2026-09-25 15:52:01 +0300

    Add files via upload

---
+++ freshman immigration hw.py
@@ -0,0 +1,40 @@
+def reverseString(s):
+    returnarr = []
+    
+    for i in range(len(s)-1,-1, -1):
+        
+        returnarr.append(s[i])
+    
+    return returnarr
+                         
+                         
+
+
+
+
+def twoSum(s, target):
+    
+    
+    indices = []
+    
+    for i in range(len(s)):
+        num = s[i]
+        sumnum = num
+        for z in range(i+1, len(s)):
+            
+            if sumnum+s[z] == target:
+                
+                indices.append(i)
+                indices.append(z)
+                
+                
+                break
+            
+            
+    return indices
+
+
+
+
+
+
